25a793cf7bd8501c7fbf4191d7350bc9ecea2e3c
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2022-08-14 09:03:49 +0000

    Changes for August 2022

--- buildbot_unified.sh
+++ buildbot_unified.sh
@@ -69,7 +69,6 @@ prep_build() {
     repopick 321339 # Allow disabling USB notifications
     repopick 329229 -f # Alter model name to avoid SafetyNet HW attestation enforcement
     repopick 329230 -f # keystore: Block key attestation for SafetyNet
-    repopick 329409 # SystemUI: screenshot: open the screenshot instead of edit
     repopick 331534 -f # SystemUI: Add support to add/remove QS tiles with one tap
     repopick 331791 # Skip checking SystemUI's permission for observing sensor privacy
 }

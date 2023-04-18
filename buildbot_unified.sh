99612391a8f942baa7d4cfe771db51ebd809167e
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2023-04-18 12:47:53 +0000

    Changes for April 2023

--- buildbot_unified.sh
+++ buildbot_unified.sh
@@ -65,7 +65,7 @@ prep_build() {
     repopick 321337 -f # Deprioritize important developer notifications
     repopick 321338 -f # Allow disabling important developer notifications
     repopick 321339 -f # Allow disabling USB notifications
-    #repopick 340916 # SystemUI: add burnIn protection
+    repopick 340916 # SystemUI: add burnIn protection
     repopick 342860 # codec2: Use numClientBuffers to control the pipeline
     repopick 342861 # CCodec: Control the inputs to avoid pipeline overflow
     repopick 342862 # [WA] Codec2: queue a empty work to HAL to wake up allocation thread

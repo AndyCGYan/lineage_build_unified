2ace4e4189ed660fe2f12e9b1ed2263b02dd7aab
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2023-12-16 19:09:44 +0800

    Changes for December 2023

--- buildbot_unified.sh
+++ buildbot_unified.sh
@@ -45,7 +45,7 @@ echo\
 )' ERR
 
 START=`date +%s`
-BUILD_DATE="$(date +%Y%m%d)"
+BUILD_DATE="$(date -u +%Y%m%d)"
 
 prep_build() {
     echo "Preparing local manifests"
@@ -62,15 +62,15 @@ prep_build() {
     mkdir -p ~/build-output
     echo ""
 
-    repopick 321337 -f # Deprioritize important developer notifications
-    repopick 321338 -f # Allow disabling important developer notifications
-    repopick 321339 -f # Allow disabling USB notifications
-    repopick 340916 # SystemUI: add burnIn protection
-    repopick 342860 # codec2: Use numClientBuffers to control the pipeline
-    repopick 342861 # CCodec: Control the inputs to avoid pipeline overflow
-    repopick 342862 # [WA] Codec2: queue a empty work to HAL to wake up allocation thread
-    repopick 342863 # CCodec: Use pipelineRoom only for HW decoder
-    repopick 342864 # codec2: Change a Info print into Verbose
+    repopick 321337 -r -f # Deprioritize important developer notifications
+    repopick 321338 -r -f # Allow disabling important developer notifications
+    repopick 321339 -r -f # Allow disabling USB notifications
+    repopick 340916 -r # SystemUI: add burnIn protection
+    repopick 342860 -r # codec2: Use numClientBuffers to control the pipeline
+    repopick 342861 -r # CCodec: Control the inputs to avoid pipeline overflow
+    repopick 342862 -r # [WA] Codec2: queue a empty work to HAL to wake up allocation thread
+    repopick 342863 -r # CCodec: Use pipelineRoom only for HW decoder
+    repopick 342864 -r # codec2: Change a Info print into Verbose
 }
 
 apply_patches() {
